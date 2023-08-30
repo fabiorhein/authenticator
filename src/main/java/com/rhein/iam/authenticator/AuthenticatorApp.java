b6a7db55b671c660b9d9c9d6f06acf1bc215a0c5
Fabio Rhein <fabiorhein@gmail.com>
2023-08-30 19:27:03 -0300

    feature/setup - Made the first configuration for IAM Authenticator

---
+++ src/main/java/com/rhein/iam/authenticator/AuthenticatorApp.java
@@ -0,0 +1,13 @@
+package com.rhein.iam.authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorApp.class, args);
+	}
+
+}
