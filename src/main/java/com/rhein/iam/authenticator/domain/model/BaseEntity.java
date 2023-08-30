41f28ac7e67b1b0fae91fade691f3f8de706de37
Fabio Rhein <fabiorhein@gmail.com>
2023-08-30 20:06:05 -0300

    feature/domain - Created User entity, service and repository

---
+++ src/main/java/com/rhein/iam/authenticator/domain/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.rhein.iam.authenticator.domain.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+}
---
+++ src/main/java/com/rhein/iam/authenticator/domain/model/User.java
@@ -0,0 +1,57 @@
+package com.rhein.iam.authenticator.domain.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends BaseEntity implements UserDetails {
+    private String fullName;
+    private String username;
+    private String password;
+    private String authorities;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
